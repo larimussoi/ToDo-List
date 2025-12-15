216bf9b36b27fa7b167c216ad28b1dc5bc1df1f5
Larissa Mussoi <larimu97@gmail.com>
2025-12-15 20:42:36 -0300

    Alterações de caixa de diálogo

---
+++ src/components/Dialog/index.jsx
@@ -0,0 +1,11 @@
+export function Dialog() {
+  return (
+    <>
+      <dialog>
+        <button autofocus>Close</button>
+        <p>This modal </p>
+      </dialog>
+      <button>Show the dialog</button>
+    </>
+  );
+}
